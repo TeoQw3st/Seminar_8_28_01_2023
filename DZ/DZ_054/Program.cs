2f9128790d72153a083ad5e50cca7a21a062b785
Pavel <Qw3st@mail.ru>
2023-01-28 18:51:41 +0300

    .\DZ_054\

--- DZ/DZ_054/Program.cs
+++ DZ/DZ_054/Program.cs
@@ -7,3 +7,58 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+int[,] CreateRandomArray(int line, int column, int start, int end)
+{
+    int[,] RandomArray = new int[line, column];
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            RandomArray[i, j] = new Random().Next(start, end + 1);
+        }
+    }
+    return RandomArray;
+}
+
+// функиця показа массива
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortingArray(int[,] array)
+{
+    int min = array [0,0];
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int y=0; y<array.GetLength(1)-1;y++){
+            if (array[i,y] < array[i,y+1])
+            {
+                int temp = array[i,y];
+                array[i, y] = array[i,y+1];
+                array[i,y+1]=temp;
+                
+            }
+            }
+
+        }
+    }
+}
+
+int[,] matr = CreateRandomArray(4, 4, 1, 12);
+ShowArray(matr);
+Console.WriteLine();
+SortingArray(matr);
+ShowArray(matr);
