df2d7557f47d8dab82cf6915ab4fcc55c0617dce
Pavel <Qw3st@mail.ru>
2023-02-03 20:09:00 +0300

    dz_062

--- DZ/DZ_062/Program.cs
+++ DZ/DZ_062/Program.cs
@@ -4,3 +4,43 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matr = new int[10, 10];
+ShowArray(matr);
+Console.WriteLine();
+int summ = 1;
+int j = 0;
+int i = 0;
+
+while (summ <= matr.GetLength(0) * matr.GetLength(1))
+{
+    matr[i, j] = summ;
+    summ++;
+
+    if (i <= j + 1 && i + j < matr.GetLength(1) - 1)
+        j++;
+
+    else if (i < j && i + j >= matr.GetLength(0) - 1)
+        i++;
+
+    else if (i >= j && i + j > matr.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+
+}
+
+
+
+ShowArray(matr);
